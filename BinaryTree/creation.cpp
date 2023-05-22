22d02da09bacdfd3be1915797b3cf1f75561120d
Lucky4604 <luckygovindrao182@gmail.com>
2023-05-22 19:21:59 +0530

    psedu code for creating binary tree

---
+++ BinaryTree/creation.cpp
@@ -0,0 +1,14 @@
+Struct Node{
+    int data;
+    Struct Node* right;
+    Struct Node* left;
+
+    //method to initaialize that value
+    Node (int val){
+        data=val;
+        right=left=NULL;
+    }
+};
+main(){
+    
+}
